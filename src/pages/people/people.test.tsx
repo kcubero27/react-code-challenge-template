44bbb289bd54b2abd5648cb4087bb687b5093d91
Karen Cubero <cuberokaren@gmail.com>
2023-02-17 23:27:35 +0100

    feat(a11y): add eslint rule, jest match and console logs when running the app to check a11 #11 (#18)

--- src/pages/people/people.test.tsx
+++ src/pages/people/people.test.tsx
@@ -1,8 +1,15 @@
 import { renderWithWrapper } from "@/tests/client-wrapper";
 import { People } from "@pages/people/people";
 import { screen } from "@testing-library/react";
+import { axe } from "jest-axe";
 
 describe("something truthy and falsy", () => {
+  it("should render it without accessibility issues", async () => {
+    const { container } = renderWithWrapper(<People />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
   it("true to be true", () => {
     renderWithWrapper(<People />);
 
